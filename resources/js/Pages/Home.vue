d8ff2222ed714300eda207d2b332a750ecf64a93
nitai-b <nitaibarran5@gmail.com>
2024-04-10 17:37:54 -0400

    the group item isn't working in the group list component idk why tf, and also rendering the group list on the Home page

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,9 +1,22 @@
 <script setup>
-import { Head, Link } from '@inertiajs/vue3';
-
+import { Head, Link } from "@inertiajs/vue3";
+import GroupList from "@/Components/app/GroupList.vue";
+import GroupItem from "@/Components/app/GroupItem.vue";
 defineProps();
 </script>
 
 <template>
     <Head title="Social Media Website" />
+    <div class="grid grid-cols-12 gap-3">
+        <div class="col-span-3 py-6 px-3">
+            <GroupList />
+            <GroupItem
+                image="https://picsum.photos/100"
+                title="Laravel Developers"
+                description="laravel developers lorem ipsum"
+            />
+        </div>
+        <div class="col-span-6">Posts</div>
+        <div class="col-span-3">Followers</div>
+    </div>
 </template>
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->middleware(['auth', 'verified'])->name('home');
+Route::get('/', [HomeController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
