b981056856a61e32669dfe36334340de67e99dad
nitai-b <nitaibarran5@gmail.com>
2024-04-10 17:36:52 -0400

    created custom components for the groups section

---
+++ resources/js/Components/app/GroupItem.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+    image: String,
+    title: String,
+    description: String,
+});
+</script>
+
+<template>
+    <div class="mb-3 cursor-pointer hover:bg-gray-100 rounded-e">
+        <div class="flex items-center gap-3 py-2 px-3">
+            <img :src="image" class="rounded-full w-[32px]" />
+            <div>
+                <h3 class="font-black text-lg">{{ title }}</h3>
+                <div class="text-xs text-gray-500">
+                    {{ description }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
