7be8a690f4289f4c1ba014876e591f1b676e2ee0
nitai-b <nitaibarran5@gmail.com>
2024-04-10 18:18:05 -0400

    changed styling in group list so that everything lines up properly with some padding in x-axis

--- resources/js/Components/app/GroupItem.vue
+++ resources/js/Components/app/GroupItem.vue
@@ -8,7 +8,7 @@ defineProps({
 
 <template>
     <div class="mb-3 cursor-pointer hover:bg-gray-100 rounded-e">
-        <div class="flex items-center gap-3 py-2 px-3">
+        <div class="flex items-center gap-3 py-2">
             <img :src="image" class="rounded-full w-[32px]" />
             <div>
                 <h3 class="font-black text-lg">{{ title }}</h3>
--- resources/js/Components/app/GroupList.vue
+++ resources/js/Components/app/GroupList.vue
@@ -7,25 +7,27 @@ const searchKeyword = ref("");
 </script>
 
 <template>
-    <h2 class="text-2xl font-bold px-3">My Groups</h2>
-    <TextInput v-model="searchKeyword" placeholder="type to search" />
-    <div class="py-8">
-        <div v-if="false">
-            <div class="text-gray-400 flex text-center justify-center">
-                You have not joined any groups
+    <div class="px-3">
+        <h2 class="text-2xl font-bold mb-4">My Groups</h2>
+        <TextInput v-model="searchKeyword" placeholder="type to search" />
+        <div class="py-8">
+            <div v-if="false">
+                <div class="text-gray-400 flex text-center justify-center">
+                    You have not joined any groups
+                </div>
             </div>
-        </div>
-        <div v-else="">
-            <GroupItem
-                image="https://picsum.photos/100"
-                title="Laravel Developers"
-                description="laravel developers lorem ipsum"
-            />
-            <GroupItem
-                image="https://picsum.photos/100"
-                title="Vue Developers"
-                description="laravel developers lorem ipsum"
-            />
+            <div v-else="">
+                <GroupItem
+                    image="https://picsum.photos/100"
+                    title="Laravel Developers"
+                    description="laravel developers lorem ipsum"
+                />
+                <GroupItem
+                    image="https://picsum.photos/100"
+                    title="Vue Developers"
+                    description="laravel developers lorem ipsum"
+                />
+            </div>
         </div>
     </div>
 </template>
