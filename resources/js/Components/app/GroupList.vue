46ff17e2e713b0efc4ec390982159b68ac74fe8c
nitai-b <nitaibarran5@gmail.com>
2024-04-10 17:43:12 -0400

    it works without the group list component

--- resources/js/Components/app/GroupList.vue
+++ resources/js/Components/app/GroupList.vue
@@ -3,24 +3,4 @@ import TextInput from "@/Components/TextInput.vue";
 import GroupItem from "@/Components/app/GroupItem.vue";
 </script>
 
-<template>
-    <h2 class="text-2xl font-bold px-3">My Groups</h2>
-    <div class="py-8">
-        <div v-if="true" class="text-gray-400 flex text-center">
-            You have not joined any groups
-        </div>
-        <div v-else="">
-            You have not joined any groups
-            <GroupItem
-                image="https://picsum.photos/100"
-                title="Laravel Developers"
-                description="laravel developers lorem ipsum"
-            />
-            <GroupItem
-                image="https://picsum.photos/101"
-                title="Vuejs Developers"
-                description="vuejs developers lorem ipsum"
-            />
-        </div>
-    </div>
-</template>
+<template></template>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -9,12 +9,21 @@ defineProps();
     <Head title="Social Media Website" />
     <div class="grid grid-cols-12 gap-3">
         <div class="col-span-3 py-6 px-3">
-            <GroupList />
-            <GroupItem
-                image="https://picsum.photos/100"
-                title="Laravel Developers"
-                description="laravel developers lorem ipsum"
-            />
+            <h2 class="text-2xl font-bold px-3">My Groups</h2>
+            <div class="py-8">
+                <div v-if="false">
+                    <div class="text-gray-400 flex text-center justify-center">
+                        You have not joined any groups
+                    </div>
+                </div>
+                <div v-else="">
+                    <GroupItem
+                        image="https://picsum.photos/100"
+                        title="Vue Developers"
+                        description="laravel developers lorem ipsum"
+                    />
+                </div>
+            </div>
         </div>
         <div class="col-span-6">Posts</div>
         <div class="col-span-3">Followers</div>
