10d2462fc537a3218c79bb6f9f8b6b5f01ad0344
nitai-b <nitaibarran5@gmail.com>
2024-04-13 15:09:38 -0400

    showing images and documents as attachments with some styling and download buttons

--- resources/js/Components/app/PostItem.vue
+++ resources/js/Components/app/PostItem.vue
@@ -3,33 +3,104 @@ import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
 const props = defineProps({
     post: Object,
 });
+
+function isImage(attachment) {
+    const mime = attachment.mime.split("/");
+    return mime[0].toLowerCase() === "image";
+}
 </script>
 
 <template>
-    <div class="bg-white border rounded p-4">
-        <div class="flex items-center gap-2 mb-3">
+    <div class="bg-white border rounded p-4 my-2">
+        <div class="flex gap-2 mb-3">
             <a href="javascript:void(0)">
-                <img class="w-[32px] rounded-full" :src="post.user.avatar" />
+                <img
+                    class="w-[32px] rounded-full border-2 transition-all hover:border-blue-400"
+                    :src="post.user.avatar"
+                />
             </a>
             <div>
-                <a href="javascript:void(0)">
-                    <h4 class="font-bold">{{ post.user.name }}</h4>
-                </a>
+                <h4 class="font-bold">
+                    <a class="hover:underline" href="javascript:void(0)">
+                        {{ post.user.name }}
+                    </a>
+                    >
+                    <a
+                        class="hover:underline"
+                        v-if="post.group !== null"
+                        href="javascript:void(0)"
+                    >
+                        {{ post.group.name }}
+                    </a>
+                </h4>
+
                 <small class="text-gray-400">{{ post.created_at }}</small>
             </div>
         </div>
         <div>
             <Disclosure v-slot="{ open }">
-                <div v-if="!open" v-html="post.body.substring(0, 50)" />
+                <div v-if="!open" v-html="post.body.substring(0, 50) + '...'" />
                 <DisclosurePanel class="">
                     <div v-html="post.body" />
                 </DisclosurePanel>
-                <div class="flex justify-end">
-                    <DisclosureButton class="text-blue-500">
+                <div class="flex justify-end mb-3">
+                    <DisclosureButton class="text-blue-500 hover:underline">
                         {{ open ? "Read less" : "Read more" }}
                     </DisclosureButton>
                 </div>
             </Disclosure>
         </div>
+        <div
+            v-if="post.attachments !== null"
+            class="grid grid-cols-2 lg:grid-cols-3 gap-3"
+        >
+            <div v-for="attachment of post.attachments">
+                <div
+                    class="aspect-square bg-blue-50 flex flex-col justify-center items-center relative"
+                >
+                    <button
+                        class="absolute w-8 h-8 bg-gray-400 right-2 top-2 cursor-pointer rounded flex items-center justify-center hover:bg-gray-700 text-white"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="black"
+                            class="w-4 h-4"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+                            />
+                        </svg>
+                    </button>
+                    <img
+                        v-if="isImage(attachment)"
+                        :src="attachment.url"
+                        class="object-cover aspect-square"
+                    />
+                    <div v-else>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="w-16 h-16"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z"
+                            />
+                        </svg>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div><button>like</button></div>
+        <div><button>comment</button></div>
     </div>
 </template>
--- resources/js/Components/app/PostList.vue
+++ resources/js/Components/app/PostList.vue
@@ -10,6 +10,39 @@ const post1 = {
     },
     body: `<p>some text for teh body lorem ipsum</p> <p>some text for teh body lorem ipsum</p><p>some text for teh body lorem ipsum</p>`,
     group: null,
+    attachments: [
+        {
+            id: 1,
+            name: "test.png",
+            mime: "image/png",
+            url: "https://picsum.photos/1000",
+        },
+        {
+            id: 2,
+            name: "test.png",
+            mime: "image/png",
+            url: "https://picsum.photos/1000",
+        },
+        {
+            id: 3,
+            name: "document.docx",
+            mime: "application/document",
+            url: "",
+        },
+    ],
+    created_at: moment(new Date()).format("llll"),
+};
+const post2 = {
+    user: {
+        id: 2,
+        avatar: "https://randomuser.me/api/portraits/men/70.jpg",
+        name: "John Smith",
+    },
+    body: `<p>some more lorem ipsum</p> <p>some text for teh body lorem ipsum</p><p>some text for teh body lorem ipsum</p>`,
+    group: {
+        id: 1,
+        name: "Laravel Developers",
+    },
     created_at: moment(new Date()).format("llll"),
 };
 </script>
@@ -17,5 +50,6 @@ const post1 = {
 <template>
     <div>
         <PostItem :post="post1" />
+        <PostItem :post="post2" />
     </div>
 </template>
