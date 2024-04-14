9ba58e05dd9f318fe4b885821999118b02f76947
nitai-b <nitaibarran5@gmail.com>
2024-04-14 15:43:29 -0400

    wrapped home page with authenticated layout

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Head, Link } from "@inertiajs/vue3";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import GroupList from "@/Components/app/GroupList.vue";
 import FollowList from "@/Components/app/FollowList.vue";
 import PostList from "@/Components/app/PostList.vue";
@@ -9,18 +10,20 @@ defineProps();
 
 <template>
     <Head title="Social Media Website" />
-    <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
-        <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
-            <GroupList />
+    <AuthenticatedLayout>
+        <div class="grid lg:grid-cols-12 gap-3 p-4 h-full">
+            <div class="lg:col-span-3 lg:order-1 h-full overflow-hidden">
+                <GroupList />
+            </div>
+            <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
+                <FollowList />
+            </div>
+            <div
+                class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col"
+            >
+                <CreatePost />
+                <PostList class="flex-1" />
+            </div>
         </div>
-        <div class="lg:col-span-3 lg:order-3 h-full overflow-hidden">
-            <FollowList />
-        </div>
-        <div
-            class="lg:col-span-6 lg:order-2 h-full overflow-hidden flex flex-col"
-        >
-            <CreatePost />
-            <PostList class="flex-1" />
-        </div>
-    </div>
+    </AuthenticatedLayout>
 </template>
