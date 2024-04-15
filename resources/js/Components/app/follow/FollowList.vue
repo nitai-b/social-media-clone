0ae665a6366739a7dd768a1db316ae18256ea828
nitai-b <nitaibarran5@gmail.com>
2024-04-14 23:30:58 -0400

    duplicated same thing for followers

---
+++ resources/js/Components/app/follow/FollowList.vue
@@ -0,0 +1,79 @@
+<script setup>
+import TextInput from "@/Components/TextInput.vue";
+import FollowItem from "@/Components/app/follow/FollowItem.vue";
+import { ref } from "vue";
+
+const searchKeyword = ref("");
+</script>
+<template>
+    <TextInput
+        v-model="searchKeyword"
+        placeholder="type to search"
+        class="w-full mt-4"
+    />
+    <div class="mt-3 h-[200px] lg:flex-1 overflow-auto">
+        <div v-if="false">
+            <div class="text-gray-400 text-center justify-center p-3">
+                You have not joined any groups
+            </div>
+        </div>
+        <div v-else="">
+            <FollowItem
+                image="https://picsum.photos/100"
+                title="Laravel Developers"
+            />
+            <FollowItem
+                image="https://picsum.photos/100"
+                title="Vue Developers"
+            />
+            <FollowItem
+                image="https://picsum.photos/100"
+                title="Laravel Developers"
+            />
+            <FollowItem
+                image="https://picsum.photos/100"
+                title="Vue Developers"
+            />
+            <FollowItem
+                image="https://picsum.photos/100"
+                title="Laravel Developers"
+            />
+            <FollowItem
+                image="https://picsum.photos/100"
+                title="Vue Developers"
+            />
+            <FollowItem
+                image="https://picsum.photos/100"
+                title="Laravel Developers"
+            />
+            <FollowItem
+                image="https://picsum.photos/100"
+                title="Vue Developers"
+            />
+            <FollowItem
+                image="https://picsum.photos/100"
+                title="Laravel Developers"
+            />
+            <FollowItem
+                image="https://picsum.photos/100"
+                title="Vue Developers"
+            />
+            <FollowItem
+                image="https://picsum.photos/100"
+                title="Laravel Developers"
+            />
+            <FollowItem
+                image="https://picsum.photos/100"
+                title="Vue Developers"
+            />
+            <FollowItem
+                image="https://picsum.photos/100"
+                title="Laravel Developers"
+            />
+            <FollowItem
+                image="https://picsum.photos/100"
+                title="Vue Developers"
+            />
+        </div>
+    </div>
+</template>
--- resources/js/Components/app/follow/FollowView.vue
+++ resources/js/Components/app/follow/FollowView.vue
@@ -1,77 +1,31 @@
 <script setup>
-import TextInput from "@/Components/TextInput.vue";
-import FollowItem from "@/Components/app/follow/FollowItem.vue";
-import { ref } from "vue";
-
-const searchKeyword = ref("");
+import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
+import { ChevronUpIcon } from "@heroicons/vue/20/solid";
+import FollowList from "@/Components/app/follow/FollowList.vue";
 </script>
 
 <template>
-    <div
-        class="px-3 bg-white rounded border py-3 h-[400px] lg:h-full overflow-hidden flex flex-col"
-    >
-        <h2 class="text-2xl font-bold mb-4">My Follows</h2>
-        <TextInput
-            v-model="searchKeyword"
-            placeholder="type to search"
-            class="w-full"
-        />
-        <div class="mt-3 flex-1 overflow-auto">
-            <div v-if="false">
-                <div class="text-gray-400 text-center justify-center p-3">
-                    You aren't following anyone yet
-                </div>
-            </div>
-            <div v-else="">
-                <FollowItem
-                    image="https://picsum.photos/100"
-                    title="Laravel Developers"
-                />
-                <FollowItem
-                    image="https://picsum.photos/100"
-                    title="Vue Developers"
-                />
-                <FollowItem
-                    image="https://picsum.photos/100"
-                    title="Laravel Developers"
-                />
-                <FollowItem
-                    image="https://picsum.photos/100"
-                    title="Vue Developers"
-                />
-                <FollowItem
-                    image="https://picsum.photos/100"
-                    title="Laravel Developers"
-                />
-                <FollowItem
-                    image="https://picsum.photos/100"
-                    title="Vue Developers"
-                />
-                <FollowItem
-                    image="https://picsum.photos/100"
-                    title="Laravel Developers"
-                />
-                <FollowItem
-                    image="https://picsum.photos/100"
-                    title="Vue Developers"
-                />
-                <FollowItem
-                    image="https://picsum.photos/100"
-                    title="Laravel Developers"
-                />
-                <FollowItem
-                    image="https://picsum.photos/100"
-                    title="Vue Developers"
-                />
-                <FollowItem
-                    image="https://picsum.photos/100"
-                    title="Laravel Developers"
-                />
-                <FollowItem
-                    image="https://picsum.photos/100"
-                    title="Vue Developers"
-                />
-            </div>
+    <div class="px-3 bg-white rounded border h-full py-3 overflow-hidden">
+        <div class="block lg:hidden">
+            <Disclosure v-slot="{ open }">
+                <DisclosureButton class="w-full">
+                    <div class="flex justify-between items-center">
+                        <h2 class="text-2xl font-bold">My Follows</h2>
+                        <ChevronUpIcon
+                            :class="open ? 'rotate-180 transform' : ''"
+                            class="h-5 w-5 text-purple-500 transition-all"
+                        />
+                    </div>
+                </DisclosureButton>
+                <DisclosurePanel class="">
+                    <FollowList />
+                </DisclosurePanel>
+            </Disclosure>
+        </div>
+
+        <div class="h-full flex-col overflow-hidden hidden lg:flex">
+            <h2 class="text-2xl font-bold mb-4">My Follows</h2>
+            <FollowList />
         </div>
     </div>
 </template>
