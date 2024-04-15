f8ec981b632fbb97fef955475c2078e11d937238
nitai-b <nitaibarran5@gmail.com>
2024-04-14 23:23:34 -0400

    finished group list view

---
+++ resources/js/Components/app/group/GroupList.vue
@@ -0,0 +1,93 @@
+<script setup>
+import TextInput from "@/Components/TextInput.vue";
+import GroupItem from "@/Components/app/group/GroupItem.vue";
+import { ref } from "vue";
+
+const searchKeyword = ref("");
+</script>
+<template>
+    <TextInput
+        v-model="searchKeyword"
+        placeholder="type to search"
+        class="w-full mt-4"
+    />
+    <div class="mt-3 h-[200px] lg:flex-1 overflow-auto">
+        <div v-if="false">
+            <div class="text-gray-400 text-center justify-center p-3">
+                You have not joined any groups
+            </div>
+        </div>
+        <div v-else="">
+            <GroupItem
+                image="https://picsum.photos/100"
+                title="Laravel Developers"
+                description="laravel developers lorem ipsum"
+            />
+            <GroupItem
+                image="https://picsum.photos/100"
+                title="Vue Developers"
+                description="laravel developers lorem ipsum"
+            />
+            <GroupItem
+                image="https://picsum.photos/100"
+                title="Laravel Developers"
+                description="laravel developers lorem ipsum"
+            />
+            <GroupItem
+                image="https://picsum.photos/100"
+                title="Vue Developers"
+                description="laravel developers lorem ipsum"
+            />
+            <GroupItem
+                image="https://picsum.photos/100"
+                title="Laravel Developers"
+                description="laravel developers lorem ipsum"
+            />
+            <GroupItem
+                image="https://picsum.photos/100"
+                title="Vue Developers"
+                description="laravel developers lorem ipsum"
+            />
+            <GroupItem
+                image="https://picsum.photos/100"
+                title="Laravel Developers"
+                description="laravel developers lorem ipsum"
+            />
+            <GroupItem
+                image="https://picsum.photos/100"
+                title="Vue Developers"
+                description="laravel developers lorem ipsum"
+            />
+            <GroupItem
+                image="https://picsum.photos/100"
+                title="Laravel Developers"
+                description="laravel developers lorem ipsum"
+            />
+            <GroupItem
+                image="https://picsum.photos/100"
+                title="Vue Developers"
+                description="laravel developers lorem ipsum"
+            />
+            <GroupItem
+                image="https://picsum.photos/100"
+                title="Laravel Developers"
+                description="laravel developers lorem ipsum"
+            />
+            <GroupItem
+                image="https://picsum.photos/100"
+                title="Vue Developers"
+                description="laravel developers lorem ipsum"
+            />
+            <GroupItem
+                image="https://picsum.photos/100"
+                title="Laravel Developers"
+                description="laravel developers lorem ipsum"
+            />
+            <GroupItem
+                image="https://picsum.photos/100"
+                title="Vue Developers"
+                description="laravel developers lorem ipsum"
+            />
+        </div>
+    </div>
+</template>
--- resources/js/Components/app/group/GroupView.vue
+++ resources/js/Components/app/group/GroupView.vue
@@ -1,11 +1,7 @@
 <script setup>
 import { Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/vue";
 import { ChevronUpIcon } from "@heroicons/vue/20/solid";
-import TextInput from "@/Components/TextInput.vue";
-import GroupItem from "@/Components/app/group/GroupItem.vue";
-import { ref } from "vue";
-
-const searchKeyword = ref("");
+import GroupList from "@/Components/app/group/GroupList.vue";
 </script>
 
 <template>
@@ -22,182 +18,14 @@ const searchKeyword = ref("");
                     </div>
                 </DisclosureButton>
                 <DisclosurePanel class="">
-                    <TextInput
-                        v-model="searchKeyword"
-                        placeholder="type to search"
-                        class="w-full mt-4"
-                    />
-                    <div class="mt-3 h-[200px] lg:flex-1 overflow-auto">
-                        <div v-if="false">
-                            <div
-                                class="text-gray-400 text-center justify-center p-3"
-                            >
-                                You have not joined any groups
-                            </div>
-                        </div>
-                        <div v-else="">
-                            <GroupItem
-                                image="https://picsum.photos/100"
-                                title="Laravel Developers"
-                                description="laravel developers lorem ipsum"
-                            />
-                            <GroupItem
-                                image="https://picsum.photos/100"
-                                title="Vue Developers"
-                                description="laravel developers lorem ipsum"
-                            />
-                            <GroupItem
-                                image="https://picsum.photos/100"
-                                title="Laravel Developers"
-                                description="laravel developers lorem ipsum"
-                            />
-                            <GroupItem
-                                image="https://picsum.photos/100"
-                                title="Vue Developers"
-                                description="laravel developers lorem ipsum"
-                            />
-                            <GroupItem
-                                image="https://picsum.photos/100"
-                                title="Laravel Developers"
-                                description="laravel developers lorem ipsum"
-                            />
-                            <GroupItem
-                                image="https://picsum.photos/100"
-                                title="Vue Developers"
-                                description="laravel developers lorem ipsum"
-                            />
-                            <GroupItem
-                                image="https://picsum.photos/100"
-                                title="Laravel Developers"
-                                description="laravel developers lorem ipsum"
-                            />
-                            <GroupItem
-                                image="https://picsum.photos/100"
-                                title="Vue Developers"
-                                description="laravel developers lorem ipsum"
-                            />
-                            <GroupItem
-                                image="https://picsum.photos/100"
-                                title="Laravel Developers"
-                                description="laravel developers lorem ipsum"
-                            />
-                            <GroupItem
-                                image="https://picsum.photos/100"
-                                title="Vue Developers"
-                                description="laravel developers lorem ipsum"
-                            />
-                            <GroupItem
-                                image="https://picsum.photos/100"
-                                title="Laravel Developers"
-                                description="laravel developers lorem ipsum"
-                            />
-                            <GroupItem
-                                image="https://picsum.photos/100"
-                                title="Vue Developers"
-                                description="laravel developers lorem ipsum"
-                            />
-                            <GroupItem
-                                image="https://picsum.photos/100"
-                                title="Laravel Developers"
-                                description="laravel developers lorem ipsum"
-                            />
-                            <GroupItem
-                                image="https://picsum.photos/100"
-                                title="Vue Developers"
-                                description="laravel developers lorem ipsum"
-                            />
-                        </div>
-                    </div>
+                    <GroupList />
                 </DisclosurePanel>
             </Disclosure>
         </div>
 
         <div class="h-full flex-col overflow-hidden hidden lg:flex">
             <h2 class="text-2xl font-bold mb-4">My Groups</h2>
-            <TextInput
-                v-model="searchKeyword"
-                placeholder="type to search"
-                class="w-full"
-            />
-            <div class="mt-3 flex-1 overflow-auto">
-                <div v-if="false">
-                    <div class="text-gray-400 text-center justify-center p-3">
-                        You have not joined any groups
-                    </div>
-                </div>
-                <div v-else="">
-                    <GroupItem
-                        image="https://picsum.photos/100"
-                        title="Laravel Developers"
-                        description="laravel developers lorem ipsum"
-                    />
-                    <GroupItem
-                        image="https://picsum.photos/100"
-                        title="Vue Developers"
-                        description="laravel developers lorem ipsum"
-                    />
-                    <GroupItem
-                        image="https://picsum.photos/100"
-                        title="Laravel Developers"
-                        description="laravel developers lorem ipsum"
-                    />
-                    <GroupItem
-                        image="https://picsum.photos/100"
-                        title="Vue Developers"
-                        description="laravel developers lorem ipsum"
-                    />
-                    <GroupItem
-                        image="https://picsum.photos/100"
-                        title="Laravel Developers"
-                        description="laravel developers lorem ipsum"
-                    />
-                    <GroupItem
-                        image="https://picsum.photos/100"
-                        title="Vue Developers"
-                        description="laravel developers lorem ipsum"
-                    />
-                    <GroupItem
-                        image="https://picsum.photos/100"
-                        title="Laravel Developers"
-                        description="laravel developers lorem ipsum"
-                    />
-                    <GroupItem
-                        image="https://picsum.photos/100"
-                        title="Vue Developers"
-                        description="laravel developers lorem ipsum"
-                    />
-                    <GroupItem
-                        image="https://picsum.photos/100"
-                        title="Laravel Developers"
-                        description="laravel developers lorem ipsum"
-                    />
-                    <GroupItem
-                        image="https://picsum.photos/100"
-                        title="Vue Developers"
-                        description="laravel developers lorem ipsum"
-                    />
-                    <GroupItem
-                        image="https://picsum.photos/100"
-                        title="Laravel Developers"
-                        description="laravel developers lorem ipsum"
-                    />
-                    <GroupItem
-                        image="https://picsum.photos/100"
-                        title="Vue Developers"
-                        description="laravel developers lorem ipsum"
-                    />
-                    <GroupItem
-                        image="https://picsum.photos/100"
-                        title="Laravel Developers"
-                        description="laravel developers lorem ipsum"
-                    />
-                    <GroupItem
-                        image="https://picsum.photos/100"
-                        title="Vue Developers"
-                        description="laravel developers lorem ipsum"
-                    />
-                </div>
-            </div>
+            <GroupList />
         </div>
     </div>
 </template>
